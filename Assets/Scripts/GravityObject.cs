899ab3c337208baeb9d32dcb2d98da5ca6d73345
tinka1110 <kotituok@arcada.fi>
2024-11-22 15:21:50 +0200

    Create GravityObject.cs

---
+++ Assets/Scripts/GravityObject.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityObject : MonoBehaviour
+{
+    // Gravitationskonstanten (kan justeras i Inspector)
+    [SerializeField] private float gravitationsKonstant = 0.1f;
+
+    // Objektets massa (kan justeras i Inspector)
+    [SerializeField] private float _massa = 1f;
+
+    // Lista över alla GravityObject-objekt i scenen
+    private static List<GravityObject> gravityObjects = new List<GravityObject>();
+
+    // Rigidbody-komponent för att hantera rörelse
+    private Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        // Hämta Rigidbody2D-komponenten
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (rigidbody2D == null)
+        {
+            Debug.LogError("Rigidbody2D saknas på " + gameObject.name);
+        }
+    }
+
+    void OnEnable()
+    {
+        // Lägg till objektet i listan när det aktiveras
+        gravityObjects.Add(this);
+    }
+
+    void OnDisable()
+    {
+        // Ta bort objektet från listan när det inaktiveras
+        gravityObjects.Remove(this);
+    }
+
+    void FixedUpdate()
+    {
+        // Applicera gravitationskraft från andra objekt
+        foreach (var annatObjekt in gravityObjects)
+        {
+            if (annatObjekt != this)
+            {
+                AppliceraGravitationskraft(annatObjekt);
+            }
+        }
+    }
+
+    private void AppliceraGravitationskraft(GravityObject annatObjekt)
+    {
+        // Beräkna avståndsvektorn mellan objekten
+        Vector2 riktning = annatObjekt.transform.position - transform.position;
+
+        // Beräkna avståndets storlek
+        float avstånd = riktning.magnitude;
+
+        // Undvik division med 0
+        if (avstånd == 0)
+        {
+            return;
+        }
+
+        // Normalisera riktningen
+        riktning.Normalize();
+
+        // Beräkna gravitationskraftens storlek med Newtons gravitationslag
+        float kraft = gravitationsKonstant * (_massa * annatObjekt.massa) / (avstånd * avstånd);
+
+        // Applicera kraften på objektets Rigidbody
+        Vector2 gravitationsKraft = riktning * kraft;
+        rigidbody2D.AddForce(gravitationsKraft);
+    }
+
+    // Publik egenskap för att justera massan i andra skript
+    public float massa
+    {
+        get { return _massa; }
+        set { _massa = value; }
+    }
+}
