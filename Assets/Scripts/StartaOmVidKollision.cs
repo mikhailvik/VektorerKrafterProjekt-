7a886fa92c913a2b8bc6bf5ff215ff86c38794ac
Viktoriia Mikhailova <mikhailv@arcada.fi>
2024-11-27 23:35:10 +0200

    StartaOmVidKollision update

--- Assets/Scripts/StartaOmVidKollision.cs
+++ Assets/Scripts/StartaOmVidKollision.cs
@@ -4,11 +4,15 @@ using UnityEngine.SceneManagement; // Behövs för att ladda om scenen
 public class StartaOmVidKollision : MonoBehaviour
 {
     // Denna metod aktiveras automatiskt när en kollision sker
-    void OnCollisionEnter(Collision kollision)
+  
+      void OnCollisionEnter2D(Collision2D kollision) // Viktoriia Mikhailova bytte -> OnCollisionEnter2D
     {
         // Kontrollera om det krockade objektet har namnet "monstr"
-        if (kollision.gameObject.name == "Monstr")
+
+       // if (kollision.gameObject.name == "Monstr") Viktoriia Mikhailova bytte ->
+        if (kollision.gameObject.CompareTag("monstr") || kollision.gameObject.CompareTag("ball") || kollision.gameObject.CompareTag("hero"))
         {
+
             // Skriv ut ett meddelande i konsolen för att verifiera kollisionen
             Debug.Log("Kollision med monstr!");
 
@@ -17,3 +21,4 @@ public class StartaOmVidKollision : MonoBehaviour
         }
     }
 }
+
