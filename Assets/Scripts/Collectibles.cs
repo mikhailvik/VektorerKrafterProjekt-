c41d548e01372c8c734cf4aa17ed9200d70950f3
Viktoriia Mikhailova <mikhailv@arcada.fi>
2024-12-02 11:46:38 +0200

    update

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
+  
     // Funktion som körs när spelaren kolliderar med objektet
-    private void OnTriggerEnter2D(Collider2D other)
-    {
+    private void OnCollisionEnter2D(Collision2D other)
+    { 
+             Debug.Log("Player collided with collectible: " + gameObject.name);
         // Kontrollera om det är spelaren som kolliderar
-        if (other.CompareTag("Monstr"))
+        if (other.gameObject.tag == "monstr")
         {
+
+         
             // Dölj eller förstör objektet
-            Destroy(gameObject);
+            Destroy (gameObject);
         }
     }
 }
